cb87ea23e1e70818cd56f125263e7c5af6a2cf75
Igor Christofer Eisenhut <igor.eisenhut@pagueveloz.com.br>
2021-09-17 18:15:22 -0300

    Commit inicial

---
+++ Berkeley.Slave/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Berkeley.Slave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
